a2f0e670458cbe12b3b47bd48a71a215877b125c
Anthony Moutafian <anthonyvache2007@gmail.com>
2022-11-25 20:36:44 +0400

    second question

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-const salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130,
-}
-if(salaries.John == 100 && salaries.Ann == 160 && salaries.Pete == 130){
-console.log(salaries.John + salaries.Ann + salaries.Pete)}else(console.log(0))
+function multiplyNumeric(obj){
+    const menu = {
+       width: 200,
+       height: 300,
+       title: 'My menu'
+     }
+     menu.width*=obj
+     menu.height*=obj
+     return menu
+ }
+ let res = multiplyNumeric(2)
+ console.log(res);
