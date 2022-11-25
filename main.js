af15f9002603161eeeb4d5c67a53244bd4dd168b
Anthony Moutafian <anthonyvache2007@gmail.com>
2022-11-25 20:35:47 +0400

    first question

---
+++ main.js
@@ -0,0 +1,7 @@
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+}
+if(salaries.John == 100 && salaries.Ann == 160 && salaries.Pete == 130){
+console.log(salaries.John + salaries.Ann + salaries.Pete)}else(console.log(0))
